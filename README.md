fab38988dc0d8f5d683318d70ec8474d47c071a9
assertivist <croc+github@sdf.org>
2014-12-30 00:27:22 -0600

    Added python bindings and refactored client

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ dashkov
 =======
 
 C++ text markov chain generator
+
+
+Building
+--------
+To run a test on the sample text (Nightmare Abbey by Thomas Love Peacock):
+
+    make
+    ./dashkovgen
+
+Python Bindings
+---------------
+Build the shared object by running `python setup.py build`. The python interface is in a declarative style:
+
+    import dashkov
+    dashmod.populate('somefile.txt')
+    reply = dashkov.get_reply('Some string') # returns a string from the markov model
+    dashkov.learn('Some other string') # add a string to the model
---
+++ clientwrapper.cpp
@@ -0,0 +1,53 @@
+#include <Python.h>
+#include "client.cpp"
+
+static PyObject *
+dashkov_populate(PyObject *self, PyObject *args)
+{
+	const char *filename;
+
+	if (!PyArg_ParseTuple(args, "s", &filename))
+		return NULL;
+
+	populateChain(filename);
+
+	Py_RETURN_NONE;
+}
+
+static PyObject *
+dashkov_get_reply(PyObject *self, PyObject *args)
+{
+	const char *input;
+	int maxwords;
+
+	if(!PyArg_ParseTuple(args, "si", &input, &maxwords))
+		return NULL;
+
+	return Py_BuildValue("s", getResponse(input, maxwords));
+}
+
+static PyObject *
+dashkov_learn(PyObject *self, PyObject *args)
+{
+	const char *input;
+
+	if(!PyArg_ParseTuple(args, "s", &input))
+		return NULL;
+
+	learn(input);
+
+	Py_RETURN_NONE;
+}
+
+static PyMethodDef ClientMethods[] = {
+	{"populate", dashkov_populate, METH_VARARGS, "Read text file into markov chain"},
+	{"get_reply", dashkov_get_reply, METH_VARARGS, "Generate a reply"},
+	{"learn", dashkov_learn, METH_VARARGS, "Learn some text"},
+	{NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC
+initdashkov(void)
+{
+	(void) Py_InitModule("dashkov", ClientMethods);
+}
